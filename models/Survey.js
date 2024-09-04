8f9efbf5fa48681c44e51be70d31bcdea74cf2bc
JJLorente <joel.jimenez.lorente@gmail.com>
2024-09-04 20:47:40 +0200

    API user levels

--- models/Survey.js
+++ models/Survey.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SurveySchema = new Schema({
-    name: {
+    name: [{ 
         type: String,
-        required: false
-    },
+        required: true 
+    }],
     expirationDate: {
         type: Date,
         required: false
